f79311606c4f9c012bf860cf5335bfb148c64f74
dellyu03 <seunghwan081@gmail.com>
2024-05-31 23:01:13 +0900

    모달 팝업 구현

--- public/JS/popup.js
+++ public/JS/popup.js
@@ -2,4 +2,14 @@
 const counterText = document.getElementById("msg-counter"); 
 counterText.innerHTML = "지금까지 N명이 편지를 보냈습니다.";
 
+popupName = "writePopup"
 //모달 팝업 구현
+function popup (popupName) {
+    const page = document.getElementById(popupName);
+    page.style.display = "block";
+}
+
+writeBtn.addEventListener('click', function(){
+    const page = document.getElementById("writePopup");
+    page.style.display = "block";
+})
