04622113d26483d673176efa5b2d94ecaeb38abc
dellyu03 <seunghwan081@gmail.com>
2024-06-01 23:22:38 +0900

    stuydying

--- public/JS/popup.js
+++ public/JS/popup.js
@@ -1,15 +1,43 @@
+const writeBtn = document.getElementById("writeBtn");
+const readBtn = document.getElementById("readBtn");
+const closeBtn = document.getElementById("closeBtn");
+const background = document.getElementById("body");
+const modalBackground = document.getElementById("modalBg");
+const content = document.getElementById("content")
 
-const counterText = document.getElementById("msg-counter"); 
-counterText.innerHTML = "지금까지 N명이 편지를 보냈습니다.";
-
-popupName = "writePopup"
 //모달 팝업 구현
-function popup (popupName) {
-    const page = document.getElementById(popupName);
-    page.style.display = "block";
-}
-
 writeBtn.addEventListener('click', function(){
     const page = document.getElementById("writePopup");
+    modalBackground.style.display = "flex";
     page.style.display = "block";
+    event.stopPropagation()
 })
+
+readBtn.addEventListener('click', function(){
+    const page = document.getElementById("readPopup");
+    modalBackground.style.display = "flex";
+    page.style.display = "block";
+    event.stopPropagation()
+})
+
+content.addEventListener('click', function(){
+    modalBackground.style.display = "none";
+})
+
+
+
+
+
+function closeWriteTab() {
+    const page = document.getElementById("writePopup");
+    page.style.display = "none";
+    modalBackground.style.display = "none";
+    event.stopPropagation()
+}
+
+function closeReadTab() {
+    const page = document.getElementById("readPopup");
+    page.style.display = "none";
+    modalBackground.style.display = "none";
+    event.stopPropagation()
+}
