be6635d0686e8edafa38c932ed2b8b2e72425558
dellyu03 <seunghwan081@gmail.com>
2024-07-08 23:08:26 +0900

    change

--- src/config/config.js
+++
@@ -1,1 +0,0 @@
-export const dbPassword = 'Dbtmdghks0@';
--- src/server.js
+++ src/server.js
@@ -9,3 +9,12 @@ const __dirname = path.resolve();
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다`);
 });
+
+connection.connect((err) => {
+  if (err) {
+      console.error('데이터베이스 연결 실패:', err.stack);
+      return;
+  }
+  console.log('데이터베이스 연결 성공. 연결 ID:', connection.threadId);
+});
+
